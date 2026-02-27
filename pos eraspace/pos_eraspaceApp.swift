3eb648b0d4d023cdf66dd1d188ff3be2b5127992
Agus Siswanto <agus.siswanto@erajaya.com>
2026-02-27 15:38:54 +0700

    Initial Commit

---
+++ pos eraspace/pos_eraspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  pos_eraspaceApp.swift
+//  pos eraspace
+//
+//  Created by Agus Siswanto on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct pos_eraspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pos eraspaceTests/pos_eraspaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  pos_eraspaceTests.swift
+//  pos eraspaceTests
+//
+//  Created by Agus Siswanto on 27/02/26.
+//
+
+import Testing
+@testable import pos_eraspace
+
+struct pos_eraspaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
